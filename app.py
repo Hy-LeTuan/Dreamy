8b96b760262a994b8bfec1d77558fbf219f0200a
Le Tuan Hy <letuanhy1507@gmail.com>
2023-07-28 21:45:16 +0700

    Missed commit

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, request, render_template, redirect, flash
 import os
 import time
+import whisper
 
 app = Flask(__name__)
+
+MODEL = whisper.load_model("small")
+app.config["UPLOAD_FOLDER"] = "static/recordings/"
+app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 1024
+BASE_AUDIO = app.config["UPLOAD_FOLDER"]
 
 
 @app.route("/")
@@ -13,7 +19,7 @@ def index():
 @app.route("/audio", methods=["GET", "POST"])
 def audio():
     if request.method == "GET":
-        return render_template("happy.html")
+        return render_template("audio.html")
     else:
         if 'audio' not in request.files:
             flash("Error, file not uploaded.")
@@ -21,10 +27,21 @@ def audio():
             return redirect(request.url)
 
         uploaded_file = request.files['audio']
-        file_path = "static/recordings"
-        filename = uploaded_file.filename
-        uploaded_file.save(os.path.join(file_path, filename))
-
+        if uploaded_file.filename == "":
+            flash("Error, file not uploaded.")
+            time.sleep(50)
+            return redirect(request.url)
+        if uploaded_file:
+            numbering = len(os.listdir(app.config["UPLOAD_FOLDER"]))
+            filename = f"recording_{numbering}.wav"
+            uploaded_file.save(os.path.join(
+                app.config["UPLOAD_FOLDER"], filename))
+            result = MODEL.transcribe(os.path.join(
+                app.config["UPLOAD_FOLDER"], filename))
+            transcribe_path = os.path.join("static", "transcribes")
+            transcribe_number = len(os.listdir(transcribe_path))
+            with open(os.path.join(transcribe_path, f"transcribe_{transcribe_number}"), "r") as f:
+                f.write(result["text"] + "\n")
         return redirect("/")
 
 
