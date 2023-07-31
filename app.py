e5c524003903cf6843217efc77d38209901b26d0
Le Tuan Hy <letuanhy1507@gmail.com>
2023-07-31 13:40:40 +0700

    Update requirements.txt, route

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_session import Session
 from helpers import login_required
+import torch
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
+# INITIALIZE DATABASE
 db = SQLAlchemy()
 app = Flask(__name__)
 app.secret_key = "secret"
 
-# config
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# CONFIGURATIONS
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///instance/database.db"
 app.config["MAX_CONTENT_LENGTH"] = 1024 * 1024 * 1024
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "recordings")
 BASE_AUDIO = app.config["UPLOAD_FOLDER"]
@@ -20,6 +23,25 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 db.init_app(app)
+
+# INITIALIZE TRANSCRIPTION MODEL
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+
+    print('There are %d GPU(s) available.' % torch.cuda.device_count())
+
+    print('We will use the GPU:', torch.cuda.get_device_name(0))
+else:
+    print('No GPU available, using the CPU instead.')
+    device = torch.device("cpu")
+
+SUM_MODEL = T5ForConditionalGeneration.from_pretrained(
+    "NlpHUST/t5-small-vi-summarization")
+SUM_MODEL_TOKENIZER = T5Tokenizer.from_pretrained(
+    "NlpHUST/t5-small-vi-summarization", legacy=False)
+SUM_MODEL.to(device)
+
+# DEFINE USER TABLES
 
 
 class User(db.Model):
@@ -49,8 +71,28 @@ class Transcripts(db.Model):
 with app.app_context():
     db.create_all()
 
-# model
+# AUDIO MODEL
 MODEL = whisper.load_model("medium")
+
+# SUMMARIZE TEXT
+
+
+def summarize(src):
+    tokenized_text = SUM_MODEL_TOKENIZER.encode(
+        src, return_tensors="pt").to(device)
+    SUM_MODEL.eval()
+    summary_ids = SUM_MODEL.generate(
+        tokenized_text,
+        max_length=256,
+        num_beams=5,
+        repetition_penalty=2.5,
+        length_penalty=1.0,
+        early_stopping=True,
+        min_length=150
+    )
+    output = SUM_MODEL_TOKENIZER.decode(
+        summary_ids[0], skip_special_tokens=True)
+    return output
 
 
 @app.route("/")
@@ -90,6 +132,7 @@ def audio():
             flash("Error, file not uploaded.")
             time.sleep(50)
             return redirect(request.url)
+
         if uploaded_file:
             numbering = len(os.listdir(app.config["UPLOAD_FOLDER"]))
             filename = f"recording_{numbering}.wav"
--- testing.py
+++ testing.py
@@ -1,6 +1,6 @@
 import torch
 from transformers import T5ForConditionalGeneration, T5Tokenizer
-if torch.cuda.is_available():       
+if torch.cuda.is_available():
     device = torch.device("cuda")
 
     print('There are %d GPU(s) available.' % torch.cuda.device_count())
@@ -10,40 +10,23 @@ else:
     print('No GPU available, using the CPU instead.')
     device = torch.device("cpu")
 
-model = T5ForConditionalGeneration.from_pretrained("NlpHUST/t5-small-vi-summarization")
-tokenizer = T5Tokenizer.from_pretrained("NlpHUST/t5-small-vi-summarization")
+model = T5ForConditionalGeneration.from_pretrained(
+    "NlpHUST/t5-small-vi-summarization")
+tokenizer = T5Tokenizer.from_pretrained(
+    "NlpHUST/t5-small-vi-summarization", legacy=False)
 model.to(device)
 
-src = """Theo BHXH Việt Nam, nhiều doanh nghiệp vẫn chỉ đóng BHXH cho người lao động theo mức lương. \\\\
-Dù quy định từ 1/1/2018, tiền lương tháng đóng BHXH gồm mức lương và thêm khoản bổ sung khác. \\\\
-BHXH Việt Nam vừa có báo cáo về tình hình thực hiện chính sách BHXH thời gian qua. \\\\
-Theo đó, tình trạng nợ, trốn đóng BHXH, BHTN vẫn xảy ra ở hầu hết các tỉnh, thành. \\\\
-Thống kê tới ngày 31/12/2020, tổng số nợ BHXH, BHYT, BHTN là hơn 13.500 tỷ đồng, \\\\
-chiếm 3,35 % số phải thu, trong đó: Số nợ BHXH bắt buộc là hơn 8.600 tỷ đồng, \\\\
-nợ BHTN là 335 tỷ đồng. Liên quan tới tiền lương đóng BHXH, báo cáo của \\\\
-BHXH Việt Nam cho thấy: Nhiều doanh nghiệp vẫn chủ yếu xây dựng thang, \\\\
-bảng lương để đóng BHXH bằng mức thấp nhất. Tức là bằng mức lương tối \\\\
-thiểu vùng, cộng thêm 7 % đối với lao động đã qua đào tạo nghề và cộng \\\\
-thêm 5 % hoặc 7 % đối với lao động làm nghề hoặc công việc nặng nhọc, \\\\
-độc hại, nguy hiểm, đặc biệt nặng nhọc độc hại và nguy hiểm. Đối với \\\\
-lao động giữ chức vụ, khoảng 80 % doanh nghiệp đã xây dựng thang, \\\\
-bảng lương cụ thể theo chức danh. Đơn cử như với chức vụ giám đốc \\\\
-sản xuất, giám đốc điều hành, trưởng phòng. Còn lại các doanh nghiệp \\\\
-xây dựng đối với lao động giữ chức vụ theo thang lương, bảng lương \\\\
-chuyên môn nghiệp vụ và bảng phụ cấp chức vụ, phụ cấp trách nhiệm. \\\\
-Thống kê của BHXH Việt Nam cũng cho thấy, đa số doanh nghiệp đã đăng \\\\
-ký đóng BHXH cho người lao động theo mức lương mà không có khoản bổ \\\\
-sung khác. Mặc dù quy định từ ngày 1/1/2018, tiền lương tháng đóng BHXH \\\\
-gồm mức lương và thêm khoản bổ sung khác."""
+src = """Sáng 31/7, khu vực sạt lở tại Trạm cảnh sát giao thông Madagui (thuộc CSGT Công an Lâm Đồng) trên đèo Bảo Lộc, lực lượng cứu hộ đưa các vật dụng, tài sản trong trạm ra ngoài, sau khi khối đất đá hàng trăm tấn được giải tỏa. Bảy xe múc, xe ủi cào đống đất đá trên quốc lộ. Ở nơi diễn ra sạt lở nặng nhất, hàng chục người cùng phương tiện cào các lớp đất dày hàng mét để tiến sâu vào bên trong trạm, tìm kiếm nạn nhân cuối cùng. Lực lượng cứu hộ đã tháo dỡ khu nhà chính của trụ sở bị nghiêng. Cảnh sát đưa nhiều chó nghiệp vụ vào hiện trường tìm kiếm ở khu vực thung lũng. Khoảng 12h, thi thể nạn nhân thứ tư được tìm thấy ở dưới đống đất đá tại trạm."""
 tokenized_text = tokenizer.encode(src, return_tensors="pt").to(device)
 model.eval()
 summary_ids = model.generate(
-                    tokenized_text,
-                    max_length=256, 
-                    num_beams=5,
-                    repetition_penalty=2.5, 
-                    length_penalty=1.0, 
-                    early_stopping=True
-                )
+    tokenized_text,
+    max_length=256,
+    num_beams=5,
+    repetition_penalty=2.5,
+    length_penalty=1.0,
+    early_stopping=True,
+    min_length=150
+)
 output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
 print(output)
