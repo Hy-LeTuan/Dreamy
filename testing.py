2ec3f2744a667ec584f0657d62c1442c78611c5a
Le Tuan Hy <letuanhy1507@gmail.com>
2023-08-23 18:59:34 +0700

    test

--- testing.py
+++ testing.py
@@ -1,3 +1,5 @@
+import tiktoken
+import openai
 import requests
 api_key = "qtteFO3qd0O2HhcbTPeNu3B1qiqWJsN"
 url = "https://api.opexams.com"
@@ -23,6 +25,21 @@ body = {
 #     print(f"Answer: {q['answer']}")
 #     print(f"Options: {q['options']}")
 
-response = requests.post(
-    f"{url}/questions-generator", headers=header, json=body)
-print(response.json()["data"])
+# response = requests.post(
+#     f"{url}/questions-generator", headers=header, json=body)
+# print(response.json()["data"])
+
+api_key = "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"
+
+
+response = openai.ChatCompletion.create(
+    api_key=api_key,
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant",
+            "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
+    ]
+)
