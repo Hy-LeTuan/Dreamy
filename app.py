c78c15c5fe38e34b1adfa4c3e5b71cb574056de6
Le Tuan Hy <letuanhy1507@gmail.com>
2023-08-24 22:08:17 +0700

    Update lots of stuff

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ Session(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    role = db.Column(db.String, nullable=False)
+    # role = db.Column(db.String, nullable=False)
     username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     recordings = db.relationship("Recording", backref="user")
